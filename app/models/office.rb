5463b8bec7f0543a16a6a13d20c8d5de4ae94bfb
sami bekas <samibekas@gmail.com>
2018-08-20 16:11:58 +0200

    models added

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,3 +1,6 @@
 class Office < ApplicationRecord
   belongs_to :user
+  has_many :reviews
+  has_many :desks
+  has_many :bookings, through: :desks
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :offices
+  has_many :reviews
+  has_many :desks, through: :bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
