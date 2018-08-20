72a9fe0b0d8605ba547f2dffcd66f8dfc405634f
Matthieu Rousseau <matthieurousseau86@gmail.com>
2018-08-20 19:10:57 +0200

    Remigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_08_20_155359) do
 
   # These are extensions that must be enabled in order to support this database
@@ -72,11 +71,11 @@ ActiveRecord::Schema.define(version: 2018_08_20_155359) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo"
     t.text "description"
-    t.string "phone_number"
     t.string "first_name"
     t.string "last_name"
+    t.string "phone_number"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
