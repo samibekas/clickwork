a43f262bee82c2c5b80d73749794f9448526d579
sami bekas <samibekas@gmail.com>
2018-08-21 11:35:56 +0200

    problem resolved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,8 +19,6 @@ class ApplicationController < ActionController::Base
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
-
-    before_action :configure_permitted_parameters, if: :devise_controller?
   end
 
   def configure_permitted_parameters
