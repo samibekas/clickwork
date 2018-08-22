507f7cc7d60e1b5f1d0689fa14cbe6f2834c3db9
Matthieu Rousseau <matthieurousseau86@gmail.com>
2018-08-22 15:12:42 +0200

    No more booking without date being specified

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,11 +1,25 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/themes/airbnb.css" // Note this is important!
 
+// $("#btn-submit-date").disabled = true;
+$("#btn-submit-date").attr("disabled", true);
 
 flatpickr(".datepicker", {
     mode: "multiple",
-    dateFormat: "Y-m-d"
+    dateFormat: "Y-m-d",
+    onChange: [
+        function(selectedDates, dateStr, instance){
+            //...
+            hidebtn();
+        },
+        function(selectedDates, dateStr, instance){
+            //...
+        }
+    ],
 })
 
+function hidebtn () {
+$("#btn-submit-date").attr("disabled", false);
+}
 
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
